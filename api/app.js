df9b97216d4ceec586d591b02734ae80a0d0a57f
Petswonder <tech@petswonder.in>
2021-10-22 15:30:07 +0530

    cors

--- api/app.js
+++ api/app.js
@@ -39,13 +39,7 @@ app.use(
   })
 );
 
-app.use(cors({
-  origin: '*',
-  methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH'],
-  allowedHeaders: [
-    'Content-Type',
-  ]
-}))
+app.use(cors())
 
 // Passport config
 import defaultExport from "./src/config/passport.js";
@@ -85,7 +79,7 @@ app.use(cookieParser());
 // Global variables
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", '*'); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
   res.header("Access-Control-Allow-Credentials", true);
   // res.header("Access-Control-Allow-Headers","Access-Control-Allow-Headers")
